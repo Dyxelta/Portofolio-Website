447313f8227e90b5e397f643e6a591bf49dbd4a2
Dixon <dixonwillow23@gmail.com>
2023-09-24 19:19:35 +0700

    feat: disable incomplete form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Contact = () => {
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+  const nameController = (event) => {
+    setName(event.target.value)
+  }
+  const emailController = (event) => {
+    setEmail(event.target.value)
+  }
+  const messageController = (event) => {
+    setMessage(event.target.value)
+  }
+
   return (
     <div name='contact' className='flex justify-center items-center w-full h-screen bg-bgblack p-4 font-roboto'>
         <form method='POST' action="https://getform.io/f/c430a1f5-aefe-4899-a3e1-9f35ceb04e8c" className='flex flex-col max-w-[600px] w-full'>
@@ -8,10 +23,11 @@ const Contact = () => {
                 <h1 className='inline text-4xl font-bold border-b-4 border-primary text-offwhite'>Contact</h1>
                 <p className='text-offwhite-dark py-4'>// Fill and send me the form below or shoot me an email //</p>
             </div>
-            <input className='p-2 rounded' type="text" placeholder='Name' name="name" id="" />
-            <input className='my-4 p-2 rounded' type="text" placeholder='Email' name="email" id="" />
-            <textarea className='p-2 rounded' name="message" rows="10" placeholder='Message'></textarea>
-            <button className='flex items-center text-offwhite border-primary border-2 px-4 py-3 my-8 mx-auto hover:bg-primary hover:text-bgblack duration-300 hover:scale-110 rounded'>Send</button>
+            <input className='p-2 rounded' type="text" placeholder='Name' name="name" value={name} onChange={nameController}/>
+            <input className='my-4 p-2 rounded' type="text" placeholder='Email' name="email" value={email} onChange={emailController}/>
+            <textarea className='p-2 rounded' name="message" rows="10" placeholder='Message' value={message} onChange={messageController}></textarea>
+
+            <button className='flex items-center text-offwhite border-primary border-2 px-4 py-3 my-8 mx-auto hover:bg-primary hover:text-bgblack duration-300 hover:scale-110 rounded ' disabled={!name || !email || !message} >Send</button>
         </form>
     </div>
   )

7c6511196252e94449d5526861839f7c37e47e56
Dixon <dixonwillow23@gmail.com>
2023-09-24 19:47:28 +0700

    fix: button sizing more responsive

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,7 +9,7 @@ import Medicheck from '../assets/Medicheck.png'
 
 const Projects = () => {
   return (
-    <div name='projects' className='w-full md:h-[190vh] text-offwhite bg-bggray font-roboto'>
+    <div name='projects' className='w-full lg:h-[190vh] md:h-[120vh] text-offwhite bg-bggray font-roboto'>
         <div className='flex flex-col justify-center max-w-[1080px] w-full h-full mx-auto p-4'>
           <div className='pb-8'>
             <h1 className='inline text-4xl font-bold border-b-4 border-primary text-offwhite'>Projects</h1>
@@ -30,7 +30,7 @@ const Projects = () => {
                   <h2 className='text-xl font-semibold text-offwhite-dark'>Personal project</h2>
                   <p className='text-offwhite py-4'>A front-end long-page website design about a course selling app created using react-vite and tailwind css</p>
                 </div>
-                <a className='flex items-center justify-evenly w-[30%] md:w-[50%] border-2 text-offwhite font-semibold border-primary px-4 py-2 hover:bg-primary hover:scale-105 hover:text-bgblack duration-300 rounded cursor-pointer' href='https://github.com/Dyxelta/eduverse'>
+                <a className='flex items-center justify-evenly w-[60%] md:w-[50%] border-2 text-offwhite font-semibold border-primary px-4 py-2 hover:bg-primary hover:scale-105 hover:text-bgblack duration-300 rounded cursor-pointer' href='https://github.com/Dyxelta/eduverse'>
                   <FaGithub></FaGithub> Repository
                 </a>
               </div>
@@ -47,7 +47,7 @@ const Projects = () => {
                   <h2 className='text-xl font-semibold text-offwhite-dark'>Course final project</h2>
                   <p className='text-offwhite py-4'>A Full-stack website design about financial management system for Software Engineering course final project in 4th-semester created using laravel bootsrap tailwind and mysql</p>
                 </div>
-                <a className=' flex items-center justify-evenly w-[30%] md:w-[50%] border-2 text-offwhite font-semibold border-primary px-4 py-2 hover:bg-primary hover:scale-105 hover:text-bgblack duration-300 rounded cursor-pointer' href='https://github.com/RifianFernando/FINance-BE'>
+                <a className=' flex items-center justify-evenly w-[60%] md:w-[50%] border-2 text-offwhite font-semibold border-primary px-4 py-2 hover:bg-primary hover:scale-105 hover:text-bgblack duration-300 rounded cursor-pointer' href='https://github.com/RifianFernando/FINance-BE'>
                   <FaGithub></FaGithub> Repository
                 </a>
               </div>
@@ -64,7 +64,7 @@ const Projects = () => {
                   <h2 className='text-xl font-semibold text-offwhite-dark'>Course final project</h2>
                   <p className='text-offwhite py-4'>A simple website design page that only focuses on back-end about a K-pop album e-commerce for Pattern Software Design course final project in 4th-Semester created using ASP.NET C#</p>
                 </div>
-                <a className='flex items-center justify-evenly w-[30%] md:w-[50%] border-2 text-offwhite font-semibold border-primary px-4 py-2 hover:bg-primary hover:scale-105 hover:text-bgblack duration-300 rounded cursor-pointer' href='https://github.com/RifianFernando/PSD-Project-LAB'>
+                <a className='flex items-center justify-evenly w-[60%] md:w-[50%] border-2 text-offwhite font-semibold border-primary px-4 py-2 hover:bg-primary hover:scale-105 hover:text-bgblack duration-300 rounded cursor-pointer' href='https://github.com/RifianFernando/PSD-Project-LAB'>
                   <FaGithub></FaGithub> Repository
                 </a>
               </div>
@@ -81,7 +81,7 @@ const Projects = () => {
                   <h2 className='text-xl font-semibold text-offwhite-dark'>Course final project</h2>
                   <p className='text-offwhite py-4'>A simple html and css website design about photography business for Human and Computer Interaction course final project in 2nd-Semester</p>
                 </div>
-                <a className='flex items-center justify-evenly w-[30%] md:w-[50%] border-2 text-offwhite font-semibold border-primary px-4 py-2 hover:bg-primary hover:scale-105 hover:text-bgblack duration-300 rounded cursor-pointer' href='https://github.com/Dyxelta/Project-HCI'>
+                <a className='flex items-center justify-evenly w-[60%] md:w-[50%] border-2 text-offwhite font-semibold border-primary px-4 py-2 hover:bg-primary hover:scale-105 hover:text-bgblack duration-300 rounded cursor-pointer' href='https://github.com/Dyxelta/Project-HCI'>
                   <FaGithub></FaGithub> Repository
                 </a>
               </div>
@@ -100,7 +100,7 @@ const Projects = () => {
                   <h2 className='text-xl font-semibold text-offwhite-dark'>Course final project</h2>
                   <p className='text-offwhite py-4'>A simple html and css only website design about online university for Human and Computer Interaction course project in 2nd-Semester</p>
                 </div>
-                <a className=' flex items-center justify-evenly w-[30%] md:w-[50%] border-2 text-offwhite font-semibold border-primary px-4 py-2 hover:bg-primary hover:scale-105 hover:text-bgblack duration-300 rounded cursor-pointer' href='https://github.com/Dyxelta/Sunbic'>
+                <a className=' flex items-center justify-evenly w-[60%] md:w-[50%] border-2 text-offwhite font-semibold border-primary px-4 py-2 hover:bg-primary hover:scale-105 hover:text-bgblack duration-300 rounded cursor-pointer' href='https://github.com/Dyxelta/Sunbic'>
                   <FaGithub></FaGithub> Repository
                 </a>
               </div>
@@ -117,7 +117,7 @@ const Projects = () => {
                   <h2 className='text-xl font-semibold text-offwhite-dark'>Personal project</h2>
                   <p className='text-offwhite py-4'>A mobile application high-fidelity prototype about application to check disease from the symptomps that users experience using AI for Artificial Intelligence course final project created using Figma</p>
                 </div>
-                <a className='flex items-center justify-evenly w-[30%] md:w-[50%] border-2 text-offwhite font-semibold border-primary px-4 py-2 hover:bg-primary hover:scale-105 hover:text-bgblack duration-300 rounded cursor-pointer' href='https://www.figma.com/file/XvcjXOQNfNYZuHlodZuJM2/Medicheck?type=design&node-id=0%3A1&mode=design&t=9OSkoFUYdwNXS9o3-1'>
+                <a className='flex items-center justify-evenly w-[60%] md:w-[50%] border-2 text-offwhite font-semibold border-primary px-4 py-2 hover:bg-primary hover:scale-105 hover:text-bgblack duration-300 rounded cursor-pointer' href='https://www.figma.com/file/XvcjXOQNfNYZuHlodZuJM2/Medicheck?type=design&node-id=0%3A1&mode=design&t=9OSkoFUYdwNXS9o3-1'>
                   <FaFigma></FaFigma> Prototype
                 </a>
               </div>
